47b9abfe7eed3bb99230e0cd86d353b0fec98a90
Leonardo Sehn <leonardomsehn@gmail.com>
2024-09-06 23:51:51 -0300

    info direct by javascript

--- src/scrips.js
+++ src/scrips.js
@@ -1,1 +1,13 @@
-console.log(data[0].description);
+//console.log(data);
+
+let section = document.getElementById("search-results");
+
+console.log(section);
+
+section.innerHTML = `<div class="result-item">
+        <h2> 
+          <a href="#" target="_blank">${data[1].planet}</a>
+        </h2>
+        <p class="description">Earth is the third planet from the Sun and the only known planet to support life. It is an ocean world, with 71% of its surface covered in water.</p>
+        <a href="https://pt.wikipedia.org/wiki/Terra" target="_blank">Mais informações</a>
+      </div>`
