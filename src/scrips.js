8a2138d37f5ce3dff8040f1066475c3ffa5c216b
Leonardo Sehn <leonardomsehn@gmail.com>
2024-09-08 00:51:17 -0300

    adicionado a logica de pesquisa e filtros

--- src/scrips.js
+++ src/scrips.js
@@ -1,13 +1,42 @@
-//console.log(data);
+function pesquisar() {
 
 let section = document.getElementById("search-results");
 
-console.log(section);
+let campoPesquisa = document.getElementById("campo-pesquisa").value;
+console.log(campoPesquisa);
 
-section.innerHTML = `<div class="result-item">
+if(!campoPesquisa){
+  section.innerHTML = "Nenhum valor digitado";
+  return
+}
+
+campoPesquisa = campoPesquisa.toLowerCase();
+
+let resultados = "";
+let planet = "";
+let description = "";
+
+for (let dado of data) {
+  planet = dado.planet.toLowerCase();
+  description = dado.planet.toLowerCase();
+  if(planet.includes(campoPesquisa) || description.includes(campoPesquisa)){
+    resultados += `
+  <div class="result-item">
         <h2> 
-          <a href="#" target="_blank">${data[1].planet}</a>
+          <a href="#" target="_blank">${dado.planet}</a>
         </h2>
-        <p class="description">Earth is the third planet from the Sun and the only known planet to support life. It is an ocean world, with 71% of its surface covered in water.</p>
-        <a href="https://pt.wikipedia.org/wiki/Terra" target="_blank">Mais informações</a>
-      </div>`
+        <p class="description">${dado.description}.</p>
+        <p class="gravity">A gravidade de ${dado.planet} é de ${dado.gravity}</p> 
+      </div>
+      `
+    }
+  }
+  
+  if(!resultados) {
+    resultados = "<p>Nenhum planeta encontrado</p>";
+  }
+
+section.innerHTML = resultados;
+}
+
+
